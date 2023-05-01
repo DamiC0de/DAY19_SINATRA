7fdfd6c7f6a9cac3ebeaf9f954dea6cdd519bee5
DamiC0de <dreymidot@gmail.com>
2023-05-01 18:42:15 +0200

    add comment to controller.rb

--- projet_validant/the_gossip_project_sinatra/lib/controller.rb
+++ projet_validant/the_gossip_project_sinatra/lib/controller.rb
@@ -4,43 +4,43 @@ require 'comment'
 
 Bundler.require
 
-class ApplicationController < Sinatra::Base
-  get '/' do
-    @gossips = Gossip.all
-    erb :index
+class ApplicationController < Sinatra::Base # Définit une nouvelle classe ApplicationController héritant de Sinatra::Base
+  get '/' do # Définit une route pour la requête GET sur '/'
+    @gossips = Gossip.all # Récupère tous les gossips
+    erb :index # Affiche la vue index.erb
   end
 
-  get '/gossips/new/' do
-    erb :new_gossip
+  get '/gossips/new/' do # Définit une route pour la requête GET sur '/gossips/new/'
+    erb :new_gossip # Affiche la vue new_gossip.erb
   end
 
-  post '/gossips/new/' do
-    Gossip.new(params['gossip_author'], params['gossip_content']).save
-    redirect '/'
+  post '/gossips/new/' do # Définit une route pour la requête POST sur '/gossips/new/'
+    Gossip.new(params['gossip_author'], params['gossip_content']).save # Crée et enregistre un nouveau gossip avec les paramètres reçus
+    redirect '/' # Redirige vers la page d'accueil
   end
 
-  get '/gossips' do
-    @all_gossips = Gossip.all
-    erb :gossips_index
+  get '/gossips' do  # Définit une route pour la requête GET sur '/gossips'
+    @all_gossips = Gossip.all # Récupère tous les gossips
+    erb :gossips_index # Affiche la vue gossips_index.erb
   end
 
 
-  get '/gossips/:id' do
-    @gossip_id = params['id'].to_i
-    @gossip = Gossip.find(@gossip_id)
-    @comments = Comment.find_by_gossip_id(@gossip_id)
-    erb :gossip_show
+  get '/gossips/:id' do # Définit une route pour la requête GET sur '/gossips/:id', où :id est un paramètre dynamique
+    @gossip_id = params['id'].to_i # Convertit le paramètre 'id' en entier et l'affecte à la variable d'instance @gossip_id
+    @gossip = Gossip.find(@gossip_id) # Trouve et récupère le gossip correspondant à l'ID
+    @comments = Comment.find_by_gossip_id(@gossip_id) # Trouve et récupère les commentaires associés au gossip
+    erb :gossip_show  # Affiche la vue gossip_show.erb
   end
 
-  get '/gossips/:id/edit' do
-    @gossip = Gossip.find(params['id'].to_i)
-    erb :edit
+  get '/gossips/:id/edit' do # Définit une route pour la requête GET sur '/gossips/:id/edit', où :id est un paramètre dynamique
+    @gossip = Gossip.find(params['id'].to_i) # Trouve et récupère le gossip correspondant à l'ID
+    erb :edit # Affiche la vue edit.erb
   end
 
-  post '/gossips/:id/update' do
-    gossip = Gossip.find(params['id'].to_i)
-    gossip.update(params['gossip_author'], params['gossip_content'])
-    redirect "/gossips/#{gossip.id}"
+  post '/gossips/:id/update' do # Définit une route pour la requête POST sur '/gossips/:id/update', où :id est un paramètre dynamique
+    gossip = Gossip.find(params['id'].to_i) # Trouve et récupère le gossip correspondant à l'ID
+    gossip.update(params['gossip_author'], params['gossip_content']) # Met à jour le gossip avec les nouveaux paramètres reçus
+    redirect "/gossips/#{gossip.id}" # Redirige vers la page du gossip mis à jour
   end
 
   post '/gossips/:id/comment' do
